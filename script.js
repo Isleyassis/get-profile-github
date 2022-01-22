31efd2129b5a51dd66f1ea97657c0b637ce5b335
Isley Assis <isleyassis@gmail.com>
2022-01-22 16:02:53 -0300

    feat: first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("button")
+
+function getprofile(event) {
+    event.preventDefault
+    console.log("opaaaa")
+}
+
+button.addEventListener("click", getprofile)
