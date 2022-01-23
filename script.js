0338d6d142413857f4045057362d8e03b53ea40f
Isley Assis <isleyassis@gmail.com>
2022-01-22 23:59:28 -0300

    :rocket: first commit

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const button = document.getElementById("button")
 
 function getprofile(event) {
-    event.preventDefault
-    console.log("opaaaa")
+    event.preventDefault()
+    fetch ("https://api.github.com/users/isleyassis")
+        .then(response => response.json())
+            .then(data => {
+                const result = document.getElementById("result")
+                result.innerHTML = data.login
+            })
 }
 
 button.addEventListener("click", getprofile)
