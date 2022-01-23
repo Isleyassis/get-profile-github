c72649641e959d200a87237a1c44ae7844984a64
Isley Assis <isleyassis@gmail.com>
2022-01-23 00:16:23 -0300

    :rocket: aplicação fazendo a requisao

--- script.js
+++ script.js
@@ -2,11 +2,32 @@ const button = document.getElementById("button")
 
 function getprofile(event) {
     event.preventDefault()
-    fetch ("https://api.github.com/users/isleyassis")
+    let input = document.getElementById("input").value
+    fetch (`https://api.github.com/users/${input}`)
         .then(response => response.json())
             .then(data => {
                 const result = document.getElementById("result")
-                result.innerHTML = data.login
+
+                const h2 = document.createElement("h2")
+                h2.innerHTML = data.login
+
+                const p = document.createElement("p")
+                p.innerHTML = data.location
+                
+                const h4 = document.createElement("h4")
+                h4.innerHTML = data.bio
+    
+                const wrapperimg = document.createElement("div")
+                wrapperimg.classList.add("wrapperimg")
+                
+                const img = document.createElement("img")
+                img.src = data.avatar_url
+
+                result.append(h2)
+                result.append(p)
+                result.append(h4)
+                result.append(wrapperimg)
+                wrapperimg.append(img)
             })
 }
 
